f9b646c46724fd35fe919e7d501f1846b07dc42b
Pits-webdev <pits.webdev@gmail.com>
2024-12-24 23:44:52 +0100

    config v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Pits-webdev.github.io',
+  site: 'https://peter-webdesign.github.io',
   integrations: [tailwind()]
 });
