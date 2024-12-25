b7675a8d936c985bbbcec8c6be62edbe7494df25
Pits-webdev <pits.webdev@gmail.com>
2024-12-25 17:53:58 +0100

    add font inter

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				accent: "var(--accent)",
+				muted: "var(--muted)",
+				border: "var(--border)",
+				input: "var(--input)",
+			},
+			fontFamily: {
+				Inter: "Inter, sans-serif",
+			},
+		},
 	},
 	plugins: [],
 }
